3bfad3dd50f935ab5f7dfd9b777059a57ef45a19
Cyrus <okikiolacyrus@gmail.com>
2025-05-10 22:35:39 +0100

    Header.js
    Beginning of Site functionality using JS. Only the Header has been made functional.
    Make the patreon menu option lead to the footer of that page.

---
+++ scripts/cart.js
@@ -0,0 +1,1 @@
+import * as header from '../scripts/header.js';
---
+++ scripts/header.js
@@ -0,0 +1,22 @@
+document.querySelector('.profile').addEventListener(('click'), () => {
+    window.location.href='../webpages/profile.html';
+})
+
+document.querySelector('.site-logo').addEventListener('click', () =>{
+    window.location.href='../webpages/home.html';
+})
+document.querySelector('.menu-profile-js').addEventListener(('click'), () => {
+    window.location.href='../webpages/profile.html';
+})
+
+document.querySelector('.menu-home-js').addEventListener('click', () =>{
+    window.location.href='../webpages/home.html';
+})
+
+document.querySelector('.menu-cart-js').addEventListener('click', () =>{
+    window.location.href='../webpages/cart.html';
+})
+
+document.querySelector('.menu-seller-js').addEventListener('click', () =>{
+    window.location.href='../webpages/profileSeller.html';
+})
